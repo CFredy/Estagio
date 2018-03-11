cf1e0fcc91fdabe1513f91173faadb464a9592ba
Fredy <cfredy@gmail.com>
2018-03-11 10:20:55 -0300

    Teste
    Commit para teste do estágio

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var tarefa = new Tarefa
+            {
+                TarefaId = "nome"
+            };
+
+
+            return View(tarefa);
+        }
+
+        [HttpPost]
+        public ActionResult Lista(Tarefa tarefa)
+        {
+
+
+            return View(tarefa);
+        }
+
+
+    }
+}
